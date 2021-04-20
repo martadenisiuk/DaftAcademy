38c08ad329b2e9dae6d3e829a97d70248c08a8b2
martadenisiuk <82670370+martadenisiuk@users.noreply.github.com>
2021-04-20 12:43:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 def root():
     return {"message": "Hello world!"}
 
-@app.api_route(path="/method", methods=["DELETE"])
+@app.api_route(path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
 def read_request(request: Request):
     return {"method": request.method}
