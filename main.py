8fe376d56525e1a681df1dca679130222e13c658
martadenisiuk <82670370+martadenisiuk@users.noreply.github.com>
2021-04-20 15:43:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
+from pydantic import BaseModel
+import hashlib
 
 app = FastAPI()
+
+class User(BaseModel):
+    password: str
+    password_hash: str
 
 @app.get("/")
 def root():
@@ -13,3 +19,17 @@ def read_request(request: Request):
 @app.api_route(path = '/method', methods=["POST"], status_code=201)
 def read_request(request: Request):
     return {"method": request.method}
+
+@app.get("/auth")
+def auth(password: str = '', password_hash: str = ''):
+    try:
+        h = hashlib.sha512(bytes(password, encoding="ASCII"))
+        if password == '' or password_hash == '':
+            status_code = 401
+        elif str(h.hexdigest()) == password_hash:
+            status_code = 204
+        else:
+            status_code = 401
+    except Exception:
+        status_code = 401
+    return Response(status_code=status_code)
