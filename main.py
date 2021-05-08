0e62605dc567827aade7c412f3bf168e76e34ca1
martadenisiuk <82670370+martadenisiuk@users.noreply.github.com>
2021-05-08 23:35:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,17 +22,18 @@ async def shutdown():
     app.db_connection.close()
     
     
+###### Zadanie 1 ##########
+    
 @app.get("/categories")
 async def categories():
     app.db_connection.row_factory = sqlite3.Row
-    categories = app.db_connection.execute('''SELECT CategoryID, CategoryName FROM Categories ORDER BY CategoryID''')\
-    .fetchall()
+    categories = app.db_connection.execute('SELECT CategoryID, CategoryName FROM Categories ORDER BY CategoryID').fetchall()
     return {
         "categories": [{'id': f"{x['CategoryID']}", 'name':f"{x['CategoryName']}"} for x in categories]
     }
 
 @app.get("/customers")
-async def products():
+async def customers():
     app.db_connection.row_factory = sqlite3.Row
     customers = app.db_connection.execute('''SELECT CustomerID, CompanyName, COALESCE(Address, '') || ' ' ||
                                           COALESCE(PostalCode, '')|| ' ' || COALESCE(City,'') || ' ' || COALESCE(Country,'') 
@@ -41,3 +42,17 @@ async def products():
         "customers": [{'id': f"{x['CustomerID']}", 'name' : f"{x['CompanyName']}", 'full_address': f"{x['address']}"}  
                       for x in customers]
     }
+
+
+###### Zadanie 2 ##########
+
+
+@app.get('/products/{id}')
+async def  products(id : int):
+    app.db_connection.row_factory = sqlite3.Row
+    products = app.db_connection.execute(f'''SELECT ProductID id, ProductName name FROM Products WHERE id = {id}''' ).fetchall()
+    try:
+        return products
+    except Exception:
+        status_code = 404
+    return Response(status_code=status_code)
