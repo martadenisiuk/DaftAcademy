90f10cafe3705630155ace89382792d911a1e31d
martadenisiuk <82670370+martadenisiuk@users.noreply.github.com>
2021-05-09 23:47:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ async def categories_post(category : Category):
     app.db_connection.row_factory = sqlite3.Row
     categories = app.db_connection.execute(
         """SELECT CategoryID id, CategoryName name FROM Categories WHERE CategoryID = ?""",(new_categories_id, )).fetchone()
-    return {'id' : int(new_categories_id) , name : category.name}
+    return categories
 
 @app.put('/categories/{id}', status_code = 200)
 async def categories_id(category : Category, id : int):
