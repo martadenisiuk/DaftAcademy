3ee0b6c6ae96a4defa64d3c510e3e6f67b9a825d
martadenisiuk <82670370+martadenisiuk@users.noreply.github.com>
2021-05-16 18:20:57 +0200

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -23,7 +23,9 @@ def get_supplier(db: Session, id: int):
 
 def get_product(db: Session, id: int):
     return (
-        db.query(models.Product, models.Category).
+        db.query(models.Product.ProductID, models.Product.ProductName,
+                    models.Category.CategoryID, models.Category.CategoryName, 
+                 models.Product.Discontinued).
         filter(models.Category.CategoryID == models.Product.CategoryID).
         filter(models.Product.SupplierID == id).
         order_by(models.Product.ProductID.desc()).all()
