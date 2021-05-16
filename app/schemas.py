e6ef0f7b0579c6cd280e5451d0f2ac559db1a261
martadenisiuk <82670370+martadenisiuk@users.noreply.github.com>
2021-05-16 20:38:06 +0200

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -32,3 +32,21 @@ class Suppliers(BaseModel):
 
     class Config:
         orm_mode = True
+        
+class ResSupplier(BaseModel):
+    SupplierID: Optional[PositiveInt]
+    CompanyName: constr(max_length=40)
+    ContactName: Optional[constr(max_length=30)]
+    ContactTitle: Optional[constr(max_length=30)]
+    Address: Optional[constr(max_length=60)]
+    City: Optional[constr(max_length=15)]
+    PostalCode: Optional[constr(max_length=10)]
+    Country: Optional[constr(max_length=15)]
+    Phone: Optional[constr(max_length=24)]
+    Fax: Optional[constr(max_length=24)]
+    HomePage: Optional[str]
+
+    class Config:
+        orm_mode = True
+        
+ 
