ee8727df06962b602ad7d024543fdf607071369b
martadenisiuk <82670370+martadenisiuk@users.noreply.github.com>
2021-05-16 16:21:11 +0200

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, PositiveInt, constr
-
+from typing import Optional
 
 class Shipper(BaseModel):
     ShipperID: PositiveInt
@@ -8,3 +8,24 @@ class Shipper(BaseModel):
 
     class Config:
         orm_mode = True
+
+class Supplier(BaseModel):
+    SupplierID: PositiveInt
+    CompanyName: constr(max_length=40)
+        
+    class Config:
+        orm_mode = True
+        
+class Suppliers(BaseModel):
+    SupplierID: PositiveInt
+    CompanyName: constr(max_length=40)
+    ContactName: constr(max_length=30)
+    ContactTitle: constr(max_length=30)
+    Address: constr(max_length=60)
+    City: constr(max_length=15)
+    Region: constr(max_length=15)
+    PostalCode: constr(max_length=15)
+    Country: constr(max_length=15)
+    Phone: constr(max_length=24)
+    Fax: constr(max_length=24)
+    HomePage: str
