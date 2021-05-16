4b69e41daf2fde0d9bad185871575871e8049aa5
martadenisiuk <82670370+martadenisiuk@users.noreply.github.com>
2021-05-16 17:44:53 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -52,17 +52,18 @@ async def get_products(id: PositiveInt, db: Session = Depends(get_db)):
 
 @router.get('/suppliers/{id}/products')
 async def get_products(id: PositiveInt, db: Session = Depends(get_db)):
-    db_products = crud.get_product(db, id)
-    if not db_products:
+    try:
+        db_products = crud.get_product(db, id)
+        return [{
+            'ProductID' : product.Product.ProductID,
+            'ProductName' : product.Product.ProductName,
+            'Category' : {
+                'CategoryID' : product.Category.CategoryID,
+                'CategoryName' : product.Category.CategoryName},
+            'Discontinued' : product.Product.Discontinued}
+            for product in db_products]
+    except:
         raise HTTPException(status_code=404, detail="Supplier not found")
-    return [{
-        'ProductID' : product.Product.ProductID,
-        'ProductName' : product.Product.ProductName,
-        'Category' : {
-            'CategoryID' : product.Category.CategoryID,
-            'CategoryName' : product.Category.CategoryName},
-        'Discontinued' : product.Product.Discontinued}
-        for product in db_products]
 
 
 
