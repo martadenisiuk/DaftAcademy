da0b8dcaeda2b7c023ef45940edec34c734b7eb0
martadenisiuk <82670370+martadenisiuk@users.noreply.github.com>
2021-05-16 20:59:18 +0200

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -35,7 +35,22 @@ def get_product(db: Session, id: int):
         order_by(models.Product.ProductID.desc()).all()
         )
 
-def create_supplier(db, supplier: models.Supplier()):
-    db.add(supplier)
+
+def create_supplier(db: Session, supplier: schemas.Add_Supplier):
+    id = db.query(models.Supplier).count() + 1
+    db_supplier = models.Supplier(
+        SupplierID= id,
+        CompanyName=supplier.CompanyName,
+        ContactName=supplier.ContactName,
+        ContactTitle=supplier.ContactTitle,
+        Address=supplier.Address,
+        City=supplier.City,
+        PostalCode=supplier.PostalCode,
+        Country=supplier.Country,
+        Phone=supplier.Phone
+    )
+    db.add(db_supplier)
     db.commit()
+    db.refresh(db_supplier)
+    return db_supplier
     pass
