b7f49795e2c98354c813f118ba523edceee708c6
martadenisiuk <82670370+martadenisiuk@users.noreply.github.com>
2021-05-16 21:05:53 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -55,5 +55,6 @@ async def get_products(id: PositiveInt, db: Session = Depends(get_db)):
 
 @router.post("/suppliers", response_model=schemas.ResSupplier, status_code=201)
 async def create_supplier(supplier: schemas.Add_Supplier, db: Session = Depends(get_db)):
-    return crud.create_supplier(db, supplier)
+    crud.create_supplier(db, supplier)
+    return crud.get_suppliers(db, supplier.SupplierID)       
 
