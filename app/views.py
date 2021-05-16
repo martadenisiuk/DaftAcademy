b3d8ddd65cee1acd659d69e607f3183e52f63381
martadenisiuk <82670370+martadenisiuk@users.noreply.github.com>
2021-05-16 18:10:57 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -52,10 +52,8 @@ async def get_products(id: PositiveInt, db: Session = Depends(get_db)):
 
 @router.get('/suppliers/{id}/products')
 async def get_products(id: PositiveInt, db: Session = Depends(get_db)):
+    db_products = crud.get_product(db, id)
     try:
-        db_products = crud.get_product(db, id)
-        if len(db_products.Product.Discontinued) == 0:
-            raise HTTPException(status_code = 404, detail='Supplier not found')
         return [{
             'ProductID' : product.Product.ProductID,
             'ProductName' : product.Product.ProductName,
@@ -64,8 +62,10 @@ async def get_products(id: PositiveInt, db: Session = Depends(get_db)):
                 'CategoryName' : product.Category.CategoryName},
             'Discontinued' : product.Product.Discontinued}
             for product in db_products]
-    except Exception:
-        raise HTTPException(status_code=404, detail="Supplier not found")
+    except HTTPException:
+        raise HTTPException(status_code = 404, detail='Supplier not found')
+        
+
 
 
 
